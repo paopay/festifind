e3342cd19af400c9a76adf9fd5ed8422174c3845
Gregory Gerla <gregorygerla1@gmail.com>
2014-06-19 16:42:09 -0700

    CSS in working order for now

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -6,10 +6,18 @@
 
 	var festival = this
 	festival.artists = []
+
 	$http.get('/festivals/angular').success(function(data){	
 		festival.artists = data.result;
 	});
+	this.favoriting = function(artist){
+		if (artist.favorite == false) {
+			artist.favorite = true;
 
+		} else {
+			artist.favorite = false;
+		}
+	};
 	}]);
 
 
