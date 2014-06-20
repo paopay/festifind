fd0356f215ab4cf58bb8464ae137b6c846e71076
Gregory Gerla <gregorygerla1@gmail.com>
2014-06-19 23:57:05 -0700

    1: Doesn't create new Rdio playlist everytime, but checks if it already exists on the festifind account.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -39,24 +39,18 @@ class AdminController < ApplicationController
       rdio = Rdio.new(["5xw5hwkpeerqpmcbwmgswaya", "qfy65r6Zrw"],[access_token, access_token_secret])
       currentUser = rdio.call('currentUser')['result']
     end
-
+    
+    playlists = rdio.call('getUserPlaylists', {"user" => 's21182955',"count" => 240});
     Festival.all.each do |festival|
-      if festival.playlist_url == nil
-        name = festival.display_name
-        desc = festival.city_name
-        tracks = []
-
-        festival.artists.each do |artist|
-          new_tracks, top_track_id = Echonest.get_tracks_list(artist.song_kick_id.to_s)
-          tracks << new_tracks
-          artist.update_attribute(:top_track, top_track_id)
-        end
-
-        tracks = tracks.join(",")
-        playlists = rdio.call('createPlaylist', {"name" => name, "description" => desc, "tracks" => tracks})
-        festival.update_attributes({:playlist_url => playlists["result"]["embedUrl"], :icon => playlists["result"]["icon"]})
-        festival.save
-      end
+      playlists["result"].each do |playlist|
+      
+       if playlist["name"] == festival.display_name
+        p playlist["name"]
+         festival.update_attributes({:playlist_url => playlist["embedUrl"], :icon => playlist["icon"]})
+         festival.save
+       end
+      end 
     end
+    redirect_to root_path
   end
 end
