8cb7cc03ad72255e92480d69e6c47e7ef0d834fa
Greg Gerla <Greg@Gregs-MacBook-Pro.local>
2014-07-17 13:44:21 -0500

    add users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140620536324) do
+ActiveRecord::Schema.define(version: 20140717183032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,9 @@ ActiveRecord::Schema.define(version: 20140620536324) do
     t.datetime "updated_at"
     t.string   "fest_icon"
     t.string   "tickets_url"
+  end
+
+  create_table "users", force: true do |t|
   end
 
 end
