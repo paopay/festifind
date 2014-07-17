532ed26c0fab3092bf3d37b8742d0d18e47a3d6b
Greg Gerla <Greg@Gregs-MacBook-Pro.local>
2014-07-17 14:08:35 -0500

    fuck

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -2,18 +2,55 @@
 (function(){
 	var festival = angular.module('app', []);
 
-	festival.controller('FestivalController',['$http',function($http){
+	festival.controller('LoginController',['$http','$scope',function($http,$scope){
+  	
+	var userIsAuthenticated = true;
+	this.login = {}
+	$scope.formData = {};
+	$scope.processForm = function(){
+	console.log("dude")
+	$http.post('/users/create', $scope.formData)
+		.success(function(data) {
+	});
+	};
+	}]);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	festival.controller('FestivalController',['$http',function($http){
 	var festival = this
 	festival.artists = []
-
 	$http.get('/festivals/angular').success(function(data){	
 		festival.artists = data.result;
 	});
 	this.favoriting = function(artist){
 		if (artist.favorite == false) {
 			artist.favorite = true;
-
 		} else {
 			artist.favorite = false;
 		}
